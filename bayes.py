8991b7522322d55336353af9150b1ac580ee2928
Bo Peng <xbopeng@outlook.com>
2018-03-01 22:56:22 -0600

    Naive Bayes finished

--- bayes.py
+++ bayes.py
@@ -16,8 +16,28 @@ instance_data_trn, meta_data = af.loadarff('lymph_train.arff')
 var_ranges = [meta_data[name][1] for name in meta_data.names()]
 label_range = var_ranges[-1]
 
-prob_per_class = bf.prob_per_class(instance_data_trn, label_range)
+instance_data_test, meta_data = af.loadarff('lymph_test.arff')
+test_labels = [ins[-1] for ins in instance_data_test]
 
-aa = bf.test(instance_data_trn, var_ranges, label_range)
+test_pred, test_postPr = bf.testset_predict(instance_data_trn, instance_data_test, var_ranges, label_range)
 
-print(len(instance_data_trn))
+num_correct_pred = bf.comp_num_correct_predict(test_labels, test_pred)
+
+# print out the results
+
+# the 1st part, node and its parents
+var_names = meta_data.names()
+for i in range(len(var_names)-1):
+    print('{0} {1}'.format(var_names[i], var_names[-1]))
+
+print('')
+
+# the 2nd part, prediction, label, posteriori probability
+for i in range(len(test_pred)):
+    print('{0} {1} {2:.12f}'.format(test_pred[i], instance_data_test[i][-1], test_postPr[i]))
+
+print('')
+
+# the 3rd part, number of correct prediction
+print(str(num_correct_pred) + '\n')
+
--- bayes_func.py
+++ bayes_func.py
@@ -24,7 +24,7 @@ def read_cmdln_arg():
     return filename_trn, filename_test, n_t
 
 
-def prob_per_class(instance_data, label_range):
+def comp_Pr_perClass(instance_data, label_range):
     """
     Compute the probability of each class
     :param instance_data: training instances
@@ -36,14 +36,15 @@ def prob_per_class(instance_data, label_range):
     num_instance = len(instance_data)
 
     # probability of each class
-    prob_per_class = []
+    Pr_perClass = []
     # loop over the label range
     for lb in label_range:
         # compute the number of instances of class lb
         num_ins_lb = sum(np.array([ins[-1] == lb for ins in instance_data]))
-        prob_per_class.append(1.0 * num_ins_lb / num_instance)
+        # using laplace estimates to compute probability
+        Pr_perClass.append((num_ins_lb + 1.0) / (num_instance + len(label_range)))
 
-    return prob_per_class
+    return Pr_perClass
 
 def split_instance_on_label(instance_data, label_range):
 
@@ -59,7 +60,7 @@ def split_instance_on_label(instance_data, label_range):
     return instance_split
 
 
-def condition_prob_each_var(var_idx, instance_with_lb, var_range):
+def comp_cndPr_oneVar_oneLabel(var_idx, instance_with_lb, var_range):
     """
     Compute the probability of each possible value of the ith variable given the instance label == lb
     :param var_idx: the index of this variable in the list of var_ranges
@@ -68,32 +69,117 @@ def condition_prob_each_var(var_idx, instance_with_lb, var_range):
     :return: conditional probability of each possible value of the (var_idx)th variable given the instance label == lb
     """
 
-    # number of possible values of this variable
-    num_var_val = len(var_range)
-    num_ins_each_val = np.zeros(num_var_val)
-    condition_prob_each_var_val = np.zeros(num_var_val)
-    for ins in instance_with_lb:
-        for i in range(num_var_val): # for the ith possible value
-            if ins[var_idx] == var_range[i]:
-                num_ins_each_val[i] += 1
+    num_instance_each_val = []
+    for var_val in var_range:
+        num_instance_this_val = sum(np.array([ins[var_idx] == var_val for ins in instance_with_lb]))
+        num_instance_each_val.append(num_instance_this_val)
+
         # compute the probability of each possible value via laplace smoothing
-        condition_prob_each_var_val = [(num+1.0) / (len(instance_with_lb)+num_var_val) for num in num_ins_each_val]
+    cndPr_eachVar_val = [(num+1.0) / (len(instance_with_lb)+len(var_range)) for num in num_instance_each_val]
 
-    return condition_prob_each_var_val
-
-
-def condition_prob_all_vars(instance_with_lb, var_ranges):
-
-    condition_prob_all_var = []
-    for idx in range(len(var_ranges)):
-        condition_prob_all_var.append(condition_prob_each_var(idx, instance_with_lb, var_ranges))
-
-    return condition_prob_all_var
+    return cndPr_eachVar_val
 
 
-def test(instance_data, var_ranges, label_range):
+def comp_cndPr_allVars_oneLabel(instance_with_lb, var_ranges):
+
+    cndPr_allVars_oneLabel = []
+    for idx in range(len(var_ranges)-1): # note that the last variable is <class>, which should be excluded
+        cndPr_allVars_oneLabel.append(comp_cndPr_oneVar_oneLabel(idx, instance_with_lb, var_ranges[idx]))
+
+    return cndPr_allVars_oneLabel
+
+
+def comp_cndPr_allVars_allLabels(instance_data, var_ranges, label_range):
+    """
+    Compute conditional probability for each variable in each subset with different lables
+    :param instance_data: training instances
+    :param var_ranges: [list] range of all variables
+    :param label_range: range of all labels
+    :return: a list, each element contains conditional probability of each variable given one class label
+    """
+
     instance_split = split_instance_on_label(instance_data, label_range)
+    cndPr_allVars_allLabels = [comp_cndPr_allVars_oneLabel(ins_splt, var_ranges) for ins_splt in instance_split]
 
-    condition_prob_all_var = condition_prob_all_vars(instance_split[0], var_ranges)
+    return cndPr_allVars_allLabels
 
-    return condition_prob_all_var
+def instance_predict(instance_test, cndPr_allVars_allLabels, Pr_perLabel, var_ranges, label_range):
+    """
+    Predict the class of the test instance
+    :param test_instance: single test instance
+    :param cndPr_allVars_allLabels: [list] conditional probability for each variable given different class lables
+    :return: the class of the test instance
+    """
+
+    # for current instance, loop over all variables to compute the conditional probability give different class labels
+    # number of classes
+    num_label = len(cndPr_allVars_allLabels)
+
+    # conditional probability for each label
+    postPr_allLabels_test = []
+    # loop over all subsets
+    for i in range(num_label):
+        cndPr_allVars_oneLabel = cndPr_allVars_allLabels[i]
+        num_var = len(cndPr_allVars_oneLabel) # number of variables
+
+        cndPr_thisLabel_eachVar_test = []
+
+        # loop over all variables
+        for j in range(num_var):
+            this_var_range = var_ranges[j] # range of this variables
+            cndPr_oneVar_oneLabel = cndPr_allVars_oneLabel[j] # conditional probability of each value of this variable
+            cndPr_this_val = cndPr_oneVar_oneLabel[this_var_range.index(instance_test[j])]
+            cndPr_thisLabel_eachVar_test.append(cndPr_this_val)
+
+        # compute the posteriori probability of this test instance being classified as the ith label
+        # posteriori Pr = P(yi) * [P(a1|yi) * P(a2|yi)*... *P(an|yi)]
+        postPr = Pr_perLabel[i] * np.product(np.array(cndPr_thisLabel_eachVar_test))
+        postPr_allLabels_test.append(postPr)
+
+    # normalize the posteriori probability
+    postPr_allLabels_test = np.array(postPr_allLabels_test)
+    postPr_sum = sum(postPr_allLabels_test)
+    postPr_allLabels_test = [postPr_lb / postPr_sum for postPr_lb in postPr_allLabels_test]
+
+    # get the index of the max conditional probability
+    idx_max_postPr = np.argmax(np.array(postPr_allLabels_test))
+    # get the label corresponding to this idx
+    test_predict = label_range[idx_max_postPr]
+    test_max_postPr = postPr_allLabels_test[idx_max_postPr]
+
+    return test_predict, test_max_postPr
+
+
+def testset_predict(instance_data_trn, instance_data_test, var_ranges, label_range):
+
+    # get conditional probability for each variable given different class lables
+    cndPr_allVars_allLabels = comp_cndPr_allVars_allLabels(instance_data_trn, var_ranges, label_range)
+
+    # get probability of each class
+    Pr_perLabel = comp_Pr_perClass(instance_data_trn, label_range)
+
+    # prediction and the max posteriori probability of the entire test data set
+    testset_pred = []
+    testset_max_postPr = []
+
+    for ins_test in instance_data_test:
+        ins_test_predict, ins_test_max_postPr = instance_predict(ins_test, cndPr_allVars_allLabels, Pr_perLabel, var_ranges, label_range)
+        testset_pred.append(ins_test_predict)
+        testset_max_postPr.append(ins_test_max_postPr)
+
+    return testset_pred, testset_max_postPr
+
+def comp_num_correct_predict(instance_data_label, instance_data_prediction):
+    """
+    Compute the number of correct prediction
+    :param instance_data_label:
+    :param instance_data_prediction:
+    :return: number of correct prediction
+    """
+
+    if len(instance_data_label) == len(instance_data_prediction):
+        num_instance = len(instance_data_label)
+        num_correct_pred = sum(np.array([instance_data_label[i] == instance_data_prediction[i] for i in range(num_instance)]))
+        return num_correct_pred
+    else:
+        return 0
